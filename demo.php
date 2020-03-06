31afb7918bc432b1072069de96436a787ceeeb84
Derek Norman <dereknorman@Dereks-MBP.domain>
2020-03-06 16:18:52 -0700

    added order form with a table with contact hours

---
+++ demo.php
@@ -0,0 +1,24 @@
+<?php print "<!doctype html>
+<html lang=\"en\">
+<head>
+<title>Form Confirmation Page</title>
+</head>
+<body>
+<hl >The form information received is given below:</hl>";
+$message = "";
+foreach ($_pOST as $key=> $value){
+$message.= $key . " : " . $value . "<br>\r\n";
+}
+
+print $message; print "<hr>
+<hr>
+<hr>
+<br>
+<hr>
+<hr>
+<form action=\"#\">
+<input type=\"button\" value  = \"Back\" onclick=\"javascript:history.go(-1)\" />
+</form>
+</body>
+</html>"
+?>
